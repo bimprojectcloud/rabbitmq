226e63ead34a7b23c7b27bcc8f254e27163f0ad5
Lukas Tomcik <lukas.tomcik@bimproject.cz>
2021-10-25 13:28:19 +0200

    feat: unsubscribe all subscriptions before disconnecting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { assertQueueAndDLX } = require("./lib/assertions");
 const publishing = require("./lib/publishing");
 const { publish, reply } = require("./lib/publishing");
 const subscription = require("./lib/subscription");
-const { subscribe } = require("./lib/subscription");
+const { subscribe, unsubscribeAll } = require("./lib/subscription");
 
 /**
  * Waits for subscription and publishing clients to connect and performs
@@ -24,6 +24,7 @@ async function connect() {
  * Disconnects subscription and publishing clients.
  */
 async function disconnect() {
+    await unsubscribeAll();
     const subscriptionClient = await subscription.client;
     await subscriptionClient.disconnect();
     const publishingClient = await publishing.client;
