a9c68f97ee41dfd5298c9c77b877b762c0b32e1c
Lukas Tomcik <lukas.tomcik@bimproject.cz>
2022-03-08 15:12:18 +0100

    refactor: remove default value of exchange parameter in publish, export default exchange

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const checks = require("./lib/checks");
 const helpers = require("./lib/helpers");
 const publishing = require("./lib/publishing");
-const { publish, reply } = require("./lib/publishing");
+const { DEFAULT_EXCHANGE, publish, reply } = require("./lib/publishing");
 const subscription = require("./lib/subscription");
 const { subscribe, unsubscribeAll } = require("./lib/subscription");
 
@@ -26,6 +26,7 @@ async function disconnect() {
 }
 
 module.exports = {
+    DEFAULT_EXCHANGE,
     checks,
     connect,
     disconnect,
